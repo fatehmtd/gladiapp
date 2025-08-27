f9df2f4a9188a93682c807546e52d10665fa9fe0
Fateh Benmerzoug <fatehmtd@gmail.com>
2025-08-27 00:24:39 -0700

    adding transcription scaffolding

--- gladiapp/include/gladiapp/gladiapp_rest.hpp
+++ gladiapp/include/gladiapp/gladiapp_rest.hpp
@@ -176,7 +176,8 @@ namespace gladiapp
                 int number_of_speakers;
                 int min_speakers;
                 int max_speakers;
-                std::string toJson() const
+                std::optional<bool> enhanced;
+                nlohmann::json toJson() const
                 {
                     // Convert the DiarizationConfig object to a JSON string
                     // You can use a JSON library like nlohmann/json for this
@@ -184,7 +185,11 @@ namespace gladiapp
                     j["number_of_speakers"] = number_of_speakers;
                     j["min_speakers"] = min_speakers;
                     j["max_speakers"] = max_speakers;
-                    return j.dump();
+                    if (enhanced.has_value())
+                    {
+                        j["enhanced"] = enhanced.value() ? "true" : "false";
+                    }
+                    return j;
                 }
             };
 
@@ -196,21 +201,38 @@ namespace gladiapp
             {
                 std::string model;
                 std::vector<std::string> target_languages;
-                bool context_adaptation;
-                std::string context;
-                bool informal;
+                std::optional<bool> match_original_utterances;
+                std::optional<bool> lipsync = true;
+                std::optional<bool> context_adaptation;
+                std::optional<std::string> context;
+                std::optional<bool> informal = false;
 
-                std::string toJson() const
+                nlohmann::json toJson() const
                 {
-                    // Convert the TranslationConfig object to a JSON string
-                    // You can use a JSON library like nlohmann/json for this
                     nlohmann::json j;
                     j["model"] = model;
                     j["target_languages"] = target_languages;
-                    j["context_adaptation"] = context_adaptation;
-                    j["context"] = context;
-                    j["informal"] = informal;
-                    return j.dump();
+                    if (match_original_utterances.has_value())
+                    {
+                        j["match_original_utterances"] = match_original_utterances.value() ? "true" : "false";
+                    }
+                    if (lipsync.has_value())
+                    {
+                        j["lipsync"] = lipsync.value() ? "true" : "false";
+                    }
+                    if (context_adaptation.has_value())
+                    {
+                        j["context_adaptation"] = context_adaptation.value() ? "true" : "false";
+                    }
+                    if (context.has_value())
+                    {
+                        j["context"] = context.value();
+                    }
+                    if (informal.has_value())
+                    {
+                        j["informal"] = informal.value() ? "true" : "false";
+                    }
+                    return j;
                 }
             };
 
@@ -220,15 +242,148 @@ namespace gladiapp
              */
             struct SubtitlesConfig
             {
-                std::vector<std::string> formats;
+                enum Format
+                {
+                    SRT,
+                    VTT
+                };
+                std::vector<Format> formats;
 
-                std::string toJson() const
+                std::optional<int> maximum_characters_per_row;
+                std::optional<int> maximum_rows_per_caption;
+
+                enum Style
+                {
+                    DEFAULT,
+                    COMPLIANCE
+                };
+                std::optional<Style> style;
+
+                std::string toString() const
                 {
                     // Convert the SubtitlesConfig object to a JSON string
                     // You can use a JSON library like nlohmann/json for this
                     nlohmann::json j;
                     j["formats"] = formats;
+                    j["maximum_characters_per_row"] = maximum_characters_per_row;
+                    j["maximum_rows_per_caption"] = maximum_rows_per_caption;
+                    j["style"] = style;
                     return j.dump();
+                }
+            };
+
+            struct CustomVocabularyConfig
+            {
+                std::string value;
+                std::vector<std::string> pronunciations;
+                std::optional<float> intensity;
+                std::optional<std::string> language;
+            };
+
+            struct CallbackConfig
+            {
+                std::string url;
+
+                enum class HttpMethod
+                {
+                    POST,
+                    PUT
+                };
+                /**
+                 * HTTP method to use for the callback, only: POST, PUT are supported.
+                 */
+                HttpMethod method;
+
+                std::string toJson() const
+                {
+                    nlohmann::json j;
+                    j["url"] = url;
+                    j["method"] = (method == HttpMethod::POST) ? "POST" : "PUT";
+                    return j.dump();
+                }
+            };
+
+            struct SummarizationConfig
+            {
+                enum Type
+                {
+                    GENERAL,
+                    BULLET_POINTS,
+                    CONCISE
+                };
+                std::vector<Type> types;
+
+                std::string toJson() const
+                {
+                    nlohmann::json j;
+                    std::vector<std::string> typeStrings;
+                    for (const auto &type : types)
+                    {
+                        switch (type)
+                        {
+                        case GENERAL:
+                            typeStrings.push_back("general");
+                            break;
+                        case BULLET_POINTS:
+                            typeStrings.push_back("bullet_points");
+                            break;
+                        case CONCISE:
+                            typeStrings.push_back("concise");
+                            break;
+                        }
+                    }
+                    j["summarization_config"] = typeStrings;
+                    return j.dump();
+                }
+            };
+
+            struct CustomSpellingConfig
+            {
+                std::unordered_map<std::string, std::vector<std::string>> spelling_dictionary;
+
+                nlohmann::json toJson() const
+                {
+                    nlohmann::json j;
+                    j["spelling_dictionary"] = spelling_dictionary;
+                    return j;
+                }
+            };
+
+            struct StructuredDataExtractionConfig
+            {
+                std::vector<std::string> classes;
+
+                nlohmann::json toJson() const
+                {
+                    nlohmann::json j;
+                    j["structured_data_extraction_config"] = classes;
+                    return j;
+                }
+            };
+
+            struct AudioToLLMConfig
+            {
+                std::vector<std::string> prompts;
+
+                nlohmann::json toJson() const
+                {
+                    nlohmann::json j;
+                    j["prompts"] = prompts;
+                    return j;
+                }
+            };
+
+            struct LanguageConfig
+            {
+                std::vector<std::string> languages;
+                bool code_switching = false;
+
+                nlohmann::json toJson() const
+                {
+                    nlohmann::json j;
+                    j["languages"] = languages;
+                    j["code_switching"] = code_switching;
+                    return j;
                 }
             };
 
@@ -239,29 +394,153 @@ namespace gladiapp
             struct AudioRequest
             {
                 std::string audio_url;
-                bool diarization;
-                DiarizationConfig diarization_config;
-                bool translation;
+
+                bool customVocabulary = false;
+                std::vector<CustomVocabularyConfig> custom_vocabulary_config;
+
+                bool callback = false;
+                std::optional<CallbackConfig> callback_config;
+
+                bool subtitles = false;
+                std::optional<SubtitlesConfig> subtitles_config;
+
+                bool diarization = false;
+                std::optional<DiarizationConfig> diarization_config;
+
+                bool translation = false;
                 TranslationConfig translation_config;
-                bool subtitles;
-                SubtitlesConfig subtitles_config;
-                bool detect_language;
-                bool enable_code_switching;
+
+                bool summarization = false;
+                std::optional<SummarizationConfig> summarization_config;
+
+                bool moderation = false;
+
+                bool named_entity_recognition = false;
+
+                bool chapterization = false;
+
+                bool name_consistency = false;
+
+                bool custom_spelling = false;
+                std::optional<CustomSpellingConfig> custom_spelling_config;
+
+                bool structured_data_extraction = false;
+                std::optional<StructuredDataExtractionConfig> structured_data_extraction_config;
+
+                bool sentiment_analysis = false;
+
+                bool audio_to_llm = false;
+                std::optional<AudioToLLMConfig> audio_to_llm_config;
+
+                bool sentences = false;
+
+                bool display_mode = false;
+
+                bool punctuation_enhanced = false;
+
+                std::optional<LanguageConfig> language_config;
 
                 std::string toJson() const
                 {
-                    // Convert the AudioRequest object to a JSON string
-                    // You can use a JSON library like nlohmann/json for this
                     nlohmann::json j;
+
                     j["audio_url"] = audio_url;
-                    j["diarization"] = diarization;
-                    j["diarization_config"] = diarization_config.toJson();
-                    j["translation"] = translation;
-                    j["translation_config"] = translation_config.toJson();
+
+                    // Custom vocabulary
+                    j["custom_vocabulary"] = customVocabulary;
+                    if (customVocabulary && !custom_vocabulary_config.empty())
+                    {
+                        nlohmann::json vocab_array = nlohmann::json::array();
+                        for (const auto &vocab : custom_vocabulary_config)
+                        {
+                            nlohmann::json vocab_obj;
+                            vocab_obj["value"] = vocab.value;
+                            vocab_obj["pronunciations"] = vocab.pronunciations;
+                            if (vocab.intensity.has_value())
+                            {
+                                vocab_obj["intensity"] = vocab.intensity.value();
+                            }
+                            if (vocab.language.has_value())
+                            {
+                                vocab_obj["language"] = vocab.language.value();
+                            }
+                            vocab_array.push_back(vocab_obj);
+                        }
+                        j["custom_vocabulary_config"] = vocab_array;
+                    }
+
+                    // Callback
+                    j["callback"] = callback;
+                    if (callback && callback_config.has_value())
+                    {
+                        j["callback_config"] = nlohmann::json::parse(callback_config->toJson());
+                    }
+
+                    // Subtitles
                     j["subtitles"] = subtitles;
-                    j["subtitles_config"] = subtitles_config.toJson();
-                    j["detect_language"] = detect_language;
-                    j["enable_code_switching"] = enable_code_switching;
+                    if (subtitles && subtitles_config.has_value())
+                    {
+                        j["subtitles_config"] = nlohmann::json::parse(subtitles_config->toString());
+                    }
+
+                    // Diarization
+                    j["diarization"] = diarization;
+                    if (diarization && diarization_config.has_value())
+                    {
+                        j["diarization_config"] = diarization_config->toJson();
+                    }
+
+                    // Translation
+                    j["translation"] = translation;
+                    if (translation)
+                    {
+                        j["translation_config"] = translation_config.toJson();
+                    }
+
+                    // Summarization
+                    j["summarization"] = summarization;
+                    if (summarization && summarization_config.has_value())
+                    {
+                        j["summarization_config"] = nlohmann::json::parse(summarization_config->toJson())["summarization_config"];
+                    }
+
+                    // Boolean flags
+                    j["moderation"] = moderation;
+                    j["named_entity_recognition"] = named_entity_recognition;
+                    j["chapterization"] = chapterization;
+                    j["name_consistency"] = name_consistency;
+                    j["sentiment_analysis"] = sentiment_analysis;
+                    j["sentences"] = sentences;
+                    j["display_mode"] = display_mode;
+                    j["punctuation_enhanced"] = punctuation_enhanced;
+
+                    // Custom spelling
+                    j["custom_spelling"] = custom_spelling;
+                    if (custom_spelling && custom_spelling_config.has_value())
+                    {
+                        j["custom_spelling_config"] = custom_spelling_config->toJson()["spelling_dictionary"];
+                    }
+
+                    // Structured data extraction
+                    j["structured_data_extraction"] = structured_data_extraction;
+                    if (structured_data_extraction && structured_data_extraction_config.has_value())
+                    {
+                        j["structured_data_extraction_config"] = structured_data_extraction_config->toJson()["structured_data_extraction_config"];
+                    }
+
+                    // Audio to LLM
+                    j["audio_to_llm"] = audio_to_llm;
+                    if (audio_to_llm && audio_to_llm_config.has_value())
+                    {
+                        j["audio_to_llm_config"] = audio_to_llm_config->toJson()["prompts"];
+                    }
+
+                    // Language config
+                    if (language_config.has_value())
+                    {
+                        j["language_config"] = language_config.value().toJson();
+                    }
+
                     return j.dump();
                 }
             };
